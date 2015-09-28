bca85444cd0b92f8b3f31285ac512ae01fca59a2
Katariina Linde <klinde@greeny.cs.tlu.ee>
2015-09-28 13:39:46 +0300

    kasutaja sisse/välja logimine

--- data.php
+++ data.php
@@ -1,1 +1,20 @@
-data
+<?php
+    //kõik, mis on seotud andmetabelitega, lisamine ja tabeli kujul esitamine
+    require_once("functions.php");
+    
+    //kui kasutaja on sisse logitud, suuna teisele lehele
+    //kontrollin, kas sessiooni muutuja on olemas 
+    if(!isset($_SESSION['logged_in_user_id'])){
+        header("Location: login.php");
+    }
+    
+    //aadressireale tekkis ?logout=1
+    if(isset($_GET["logout"])){
+        //kustutame sessiooni muutujad
+        session_destroy();
+        header("Location: login.php");
+    }
+?>
+
+Tere, <?=$_SESSION['logged_in_user_email'];?> <br><a href="?logout=1">Logi välja</a>
+
