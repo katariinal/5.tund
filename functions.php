d3b5d17106bb02ecedcecdb83cf7dc74d3c3e598
Katariina Linde <klinde@greeny.cs.tlu.ee>
2015-09-28 13:11:53 +0300

    functions and global

--- functions.php
+++ functions.php
@@ -1,1 +1,39 @@
-fun
+<?php
+    //loome AB ühenduse
+    require_once("../config_global.php");
+    $database = "if15_klinde";
+    
+    
+    function logInUser($email, $hash){
+        
+        // GLOBALS saab kätte kõik muutujad mis kasutusel
+        $mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["database"]);
+        
+        $stmt = $mysqli->prepare("SELECT id, email FROM user_sample WHERE email=? AND password=?");
+        $stmt->bind_param("ss", $email, $hash);
+        $stmt->bind_result($id_from_db, $email_from_db);
+        $stmt->execute();
+        if($stmt->fetch()){
+            echo "Kasutaja logis sisse id=".$id_from_db;
+        }else{
+            echo "Wrong credentials!";
+        }
+        $stmt->close();
+        
+        $mysqli->close();
+        
+    }
+    
+    
+    function createUser($create_email, $hash){
+        
+        $mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["database"]);
+        $stmt = $mysqli->prepare("INSERT INTO user_sample (email, password) VALUES (?,?)");
+        $stmt->bind_param("ss", $create_email, $hash);
+        $stmt->execute();
+        $stmt->close();
+        
+        $mysqli->close();
+        
+    }
+ ?>
